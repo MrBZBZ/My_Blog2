205d52597607be818eff731d59a7d2fa849940e3
Mrbzbz <bzbz233nb@163.com>
2024-06-24 09:00:20 +0800

    Site updated: 2024-06-24 09:00:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/02/hwid/","2024/05/02/autologin/","2024/05/01/hello-world/","2024/06/15/zongping/"];function toRandomPost(){
+var posts=["2024/05/02/autologin/","2024/05/01/hello-world/","2024/06/15/zongping/","2024/05/02/hwid/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
