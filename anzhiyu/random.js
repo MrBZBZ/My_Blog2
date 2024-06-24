159c2ca8f2f763ca58892f8487c712579d01c079
1ea^es <121741548+MrBZBZ@users.noreply.github.com>
2024-06-24 10:32:25 +0800

    Delete anzhiyu directory

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/05/02/autologin/","2024/05/01/hello-world/","2024/06/15/zongping/","2024/05/02/hwid/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
