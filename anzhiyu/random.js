688965c6723e667729a5d3a4ac0356da3b88b435
Leaves_awa <bzbz233nb@163.com>
2025-06-25 20:38:28 +0800

    1

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/16/api-xiufu/","2024/05/02/autologin/","2024/05/02/hwid/","2024/08/09/twikoo/","2024/05/01/hello-world/","2024/06/25/valine/","2024/06/15/zongping/","2024/08/10/Bilibili-Video-Downloader/"];function toRandomPost(){
+var posts=["2024/07/16/api-xiufu/","2024/05/02/autologin/","2024/05/02/hwid/","2024/08/09/twikoo/","2024/05/01/hello-world/","2024/06/25/valine/","2024/06/15/zongping/","2024/08/10/Bilibili-Video-Downloader/","2025/06/25/obf/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
